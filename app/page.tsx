9350b80d32cadb1682dadbe48494b05fb1d5951d
muthomi254 <dmunene202020@gmail.com>
2024-04-04 19:35:03 +0300

    (ft): using links in different pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Image from 'next/image'
-
+import Link from 'next/link'
+import ProductCard from './components/ProductCard'
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     hellow world
+    <main className="flex min-h-screen flex-col  ">
+     Hellow world
+     <Link href="/users">Users</Link>
+     <Link href={'/users/new'}>New User</Link>
+     <ProductCard />
     </main>
   )
 }
